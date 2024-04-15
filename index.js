7b140c1485f7edfb65fe05cf375e4361d1c573ac
sofynaz <gstservices1236@gmail.comm>
2024-04-15 09:18:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("I am a naz");
+// var num:number = 5
+// * function define
+// @ts-ignore
+function sum1(a, b) {
+    return a + b;
+}
+console.log(sum1(3, 2));
+//* function define
---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log("I am a naz");
+// var num:number = 5
+
+// * function define
+// @ts-ignore
+function sum1(a: number,b: number):number{
+    return a+b;
+}
+
+console.log(sum1(3,2));
+
+
+//* function define
